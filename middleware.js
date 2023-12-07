e6e61214f8559891b76407f2f64d2082f2db7112
Julia Shi <julia.shi@nyu.edu>
2023-12-07 16:19:14 +0000

    response

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
+import { NextResponse } from 'next/server';
+
 export default async function middleware(req) {
   const { pathname } = req.nextUrl
  
   console.log(pathname)
+  return new NextResponse(
+    'you are in middleware',
+  );
 }
- 
