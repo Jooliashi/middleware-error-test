808f4b159bd620c3b1613afdfaf93da04fce9bd8
Julia Shi <julia.shi@nyu.edu>
2023-12-07 12:54:19 +0000

    add logs

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
  
 export function middleware() {
+  console.log('Inside middleware')
   return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
 }
  
