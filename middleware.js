f68ab3c8ff9d7d8760f4abb945387c145cc9cbf2
Julia Shi <julia.shi@nyu.edu>
2024-02-12 09:06:57 +0000

    update

--- middleware.js
+++ middleware.js
@@ -3,3 +3,7 @@ import { NextResponse } from 'next/server';
 export default async function middleware(req) {
   return NextResponse.json({ message: 'Too many requests' }, { status: 429 })
 }
+
+export const config = {
+  matcher: '/pages/api/hello.js',
+}
